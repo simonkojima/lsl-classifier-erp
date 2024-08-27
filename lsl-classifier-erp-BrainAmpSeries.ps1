ff367c7742125536fd043b0dadcb8ba3dcf38bf7
Simon Kojima <simon.kojima@outlook.com>
2024-08-27 14:34:54 +0900

    Update lsl-classifier-erp-BrainAmpSeries.ps1

--- lsl-classifier-erp-BrainAmpSeries.ps1
+++ lsl-classifier-erp-BrainAmpSeries.ps1
@@ -1,5 +1,5 @@
 conda activate classifier
 python ./main.py --marker scab-c `
                  --signal BrainAmpSeries `
-                 --log  C:\Users\simon\Documents\eeg\asme-speller\sub-C\ses-S001\log
+                 --log  C:\Users\simon\Documents\eeg\asme-speller\sub-D\ses-S001\log
 pause
